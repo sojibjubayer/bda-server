44e762ca07f08902c3cab63ac09f370b733315af
samira muniyat <sojibjubayer@gmail.com>
2023-11-27 00:27:16 +0600

    basic database with users post and get

--- index.js
+++ index.js
@@ -36,41 +36,6 @@ async function run() {
     const cartCollection = client.db("bdaDB").collection("carts");
     const paymentCollection = client.db("bdaDB").collection("payments");
 
-    //jwt related api
-    // app.post('/jwt', async (req, res) => {
-    //   const user = req.body;
-    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
-    //   res.send({ token })
-    // })
-
-    // middlewares
-    // const verifyToken = (req, res, next) => {
-    //   console.log('inside verify token', req.headers.authorization)
-    //   if (!req.headers.authorization) {
-    //     return res.status(401).send({ message: 'unauthorized access' })
-    //   }
-    //   const token = req.headers.authorization.split(' ')[1]
-    //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    //     if (err) {
-    //       return res.status(401).send({ message: 'unauthorized access' })
-    //     }
-    //     req.decoded = decoded;
-    //     next()
-    //   })
-    // }
-    // verify admin after verifyToken
-    // const verifyAdmin = async (req, res, next) => {
-    //   const email = req.decoded.email;
-    //   const query = { email: email }
-    //   const user = await userCollection.findOne(query)
-    //   const isAdmin = user?.role === 'admin'
-    //   if (!isAdmin) {
-    //     return res.status(403).send({ message: 'forbidden  access' })
-    //   }
-    //   next()
-    // }
-
-    // users related api
 
 
 
