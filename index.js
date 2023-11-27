2848b215cf61375b8028b19fbf8770586e248603
samira muniyat <sojibjubayer@gmail.com>
2023-11-27 18:25:11 +0600

    api for find data using email

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_SECRETE_KEY);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
@@ -33,9 +33,44 @@ async function run() {
     const userCollection = client.db("bdaDB").collection("users");
     const districtsCollection = client.db("bdaDB").collection("districts");
     const upazilasCollection = client.db("bdaDB").collection("upazilas");
-    const cartCollection = client.db("bdaDB").collection("carts");
+    const donationRequestCollection = client.db("bdaDB").collection("donationRequests");
     const paymentCollection = client.db("bdaDB").collection("payments");
 
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
+      res.send({ token })
+    })
+
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization)
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next()
+      })
+    }
+    // verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await userCollection.findOne(query)
+      const isAdmin = user?.role === 'admin'
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden  access' })
+      }
+      next()
+    }
+
+    
 
 
 
@@ -59,6 +94,87 @@ async function run() {
     app.get('/upazilas', async (req, res) => {
         const result = await upazilasCollection.find().toArray()
         res.send(result);
+      })
+
+    
+
+          // admin related api
+    app.get('/users/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { email: email }
+        const user = await userCollection.findOne(query)
+        let admin = false;
+        if (user) {
+          admin = user?.role === 'admin'
+        }
+        res.send({ admin })
+      })
+
+
+
+
+          // get request
+          app.get('/donationRequests', async (req, res) => {
+            const email = req.query.email;
+            const query = { reqEmail: email };
+            const result = await donationRequestCollection.find(query).toArray();
+            res.send(result);
+          });
+
+
+          app.get('/donationRequests/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await donationRequestCollection.findOne(query);
+            res.send(result);
+          })
+
+          app.patch('/donationRequests/:id', async (req, res) => {
+            const info = req.body;
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updatedDoc = {
+              $set: {
+                name: info.name,
+                category: item.category,
+                price: info.price,
+                recipe: info.recipe,
+              
+              }
+            }
+      
+            const result = await donationRequestCollection.updateOne(filter, updatedDoc)
+            res.send(result);
+          })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      //CREATE DONATION REQUEST POST
+      app.post('/donationRequest',async(req,res)=>{
+        const request=req.body;
+        const result = await donationRequestCollection.insertOne(request)
+        res.send(result)
       })
 
 
